6beec5d4524d48e6bd6d0344fe4c7777d30a0b56
jason walker <jwalkerus3@yahoo.com>
2021-10-23 11:20:46 -0500

    Freecodecamp Python for Beginners

---
+++ app1.py
@@ -0,0 +1,41 @@
+"""MadLib game
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural Noun: ")
+celebrity = input("Enter a celebrity: ")
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)"""
+
+"""Basic calculator
+num1 = input("First num: ")
+num2 = input("Second num: ")
+result = float(num1) + float(num2)
+print(result)
+"""
+
+"""Lists
+friends = ["Jason", 44, "Heather", 44, "Mikey", 16, "Dakota", 9]
+#friends[3] = 45
+print(friends)
+print(friends[0:2])"""
+
+""""#List functions
+lucky_nums = [3, 6, 7, 9, 44, 100]
+friends = ["Jason", 44, "Heather", 44, "Mikey", "Mikey", 16, "Dakota", 9]
+#friends.extend(lucky_nums)
+#friends.append("Joey")
+#friends.insert(8, "KK")
+#friends.remove("KK")
+#friends.clear()
+#friends.pop()
+#print(friends)
+#print(friends.index("Joey"))
+#print(friends.count("Mikey"))
+#lucky_nums.sort() #Error,,, friends.sort() will not sort int and string, needs more info
+#lucky_nums.reverse()
+friends2 = friends.copy()
+print(lucky_nums)
+print(friends)"""
+
+#Tuples start here
+
